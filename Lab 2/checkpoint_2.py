9d577ebdce260ac344d920bd53cc5575cd71134d
Abraar M Nafiz <abraar@Abraars-Air.local>
2024-05-07 03:37:56 +0600

    cp2 update2

--- Lab 2/checkpoint_2.py
+++ Lab 2/checkpoint_2.py
@@ -1,27 +1,6 @@
-# For the second problem, we have to deal with a substitution cipher.
-# We can attempt a simple frequency analysis to guess the substitutions based on English letter frequencies.
-# First, we'll write a function to count the frequency of each character in a cipher.
 
-def frequency_analysis(ciphertext):
+from collections import Counter
 
-    frequencies = {}
-    total_letters = 0 
-
-    for char in ciphertext:
-        if char.isalpha():
-            if char in frequencies:
-                frequencies[char] += 1
-            else:
-                frequencies[char] = 1
-            total_letters += 1
-
-    for letter in frequencies:
-        frequencies[letter] = (frequencies[letter] / total_letters) * 100
-
-    return dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
-
-
-# Two ciphers given in the problem
 cipher_sub_1 = ("af p xpkcaqvnpk pfg, af ipqe qpri, gauuikifc tpw, ceiri udvk tiki afgarxifrphni cd eao-"
                 "wvmd popkwn, hiqpvri du ear jvaql vfgikrcpfgafm du cei xkafqaxnir du xrwqedearcdkw pfg "
                 "du ear aopmafpcasi xkdhafmr afcd fit pkipr. ac tpr qdoudkcafm cd lfdt cepc au pfwceafm "
@@ -39,51 +18,29 @@ cipher_sub_2 = ("aceah toz puvg vcdl omj puvg yudqecov, omj loj auum klu thmjuv 
                 "doee lcw tuee-yvuzuvpuj; aqk qmdlomnuj thqej lopu auum muovuv klu wovr. kluvu tuvu zhwu "
                 "klok zlhhr klucv luojz omj klhqnlk klcz toz khh wqdl hs o nhhj klcmn; ck zuuwuj qmsocv klok")
 
-# Calculate frequencies for both ciphers
-frequency_1 = frequency_analysis(cipher_sub_1)
-frequency_2 = frequency_analysis(cipher_sub_2)
 
 
-# print(frequency_1)
-# print(frequency_2)
-
-
-def map_frequencies(cipher_freq, english_freq):
-    sorted_cipher = sorted(cipher_freq.items(), key=lambda item: item[1], reverse=True)
-    sorted_english = sorted(english_freq.items(), key=lambda item: item[1], reverse=True)
+def decrypt_substitution_cipher(encrypted_text):
     
-    mapping = {}
-    for cipher_char, english_char in zip(sorted_cipher, sorted_english):
-        mapping[cipher_char[0]] = english_char[0]
+    eng_freq_order = 'etaoinshrdlcumwfgypbvkjxqz'  # English letter frequency roughly sorted by frequency
     
-    return mapping
+    counter = Counter([char for char in encrypted_text if char.isalpha()])
+    total_chars = sum(counter.values())
+    freq_list = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+    
+    # Map most frequent letter in cipher to most frequent letter in English
+    cipher_to_eng = {}
+    for i, (char, count) in enumerate(freq_list):
+        cipher_to_eng[char] = eng_freq_order[i % len(eng_freq_order)]
+    
+    
+    decrypted_text = ''.join(cipher_to_eng.get(char, char) for char in encrypted_text)
+    
+    return decrypted_text, cipher_to_eng
 
-def decrypt(ciphertext, mapping):
-    plaintext = ''
-    for char in ciphertext:
-        if char in mapping:
-            plaintext += mapping[char]
-        else:
-            plaintext += char
-    return plaintext
 
-english_frequencies = {
-    'a': 8.05, 'b': 1.67, 'c': 2.23, 'd': 5.10, 'e': 12.22, 'f': 2.14,
-    'g': 2.30, 'h': 6.62, 'i': 6.28, 'j': 0.19, 'k': 0.95, 'l': 4.08,
-    'm': 2.33, 'n': 6.95, 'o': 7.63, 'p': 1.66, 'q': 0.06, 'r': 5.29,
-    's': 6.02, 't': 9.67, 'u': 2.92, 'v': 0.82, 'w': 2.60, 'x': 0.11,
-    'y': 2.04, 'z': 0.06
-}
 
-cipher_frequencies = {
-    'i': 11.33, 'd': 8.87, 'c': 8.13, 'p': 7.88, 'a': 7.64, 'f': 7.39, 'r': 5.67,
-    'e': 5.42, 'k': 4.68, 'g': 4.68, 'n': 3.94, 'q': 3.69, 'v': 3.20, 'u': 3.20,
-    't': 2.71, 'o': 2.71, 'x': 2.46, 'w': 1.97, 'm': 1.72, 'h': 1.48, 'l': 0.74,
-    'j': 0.25, 's': 0.25
-}
-
-mapping = map_frequencies(cipher_frequencies, english_frequencies)
-ciphertext = cipher_sub_2
-decrypted_text = decrypt(ciphertext, mapping)
-
-print(decrypted_text)
+encrypted_text = cipher_sub_1
+decrypted_text, key = decrypt_substitution_cipher(encrypted_text)
+print("Decryption Key:", key)
+print("Decrypted Text:", decrypted_text)
