8b170cf1cfae1fa9fc497c672d074ef30ca0ccb4
Abraar M Nafiz <abraar@Abraars-Air.local>
2024-05-07 05:11:05 +0600

    L2 cp2r

---
+++ Lab 2/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# for c in to_encrypt:
+
+#     decrypted_text, key = decrypt_substitution_cipher(c)
+#     print(f"Encrypted Text : {c}")
+#     print(f"Decryption Key: {key}")
+#     print(f"Decrypted Text: {decrypted_text}")
+#     print("\n\n\n")
